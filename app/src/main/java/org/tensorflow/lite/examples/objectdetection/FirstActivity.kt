c51f446828ce395ee9a0051ef5f27e6e6819d1d1
JayLim04 <21033869@myrp.edu.sg>
2023-08-06 16:22:30 +0800

    Added sharedpref for emergency contact

--- app/src/main/java/org/tensorflow/lite/examples/objectdetection/FirstActivity.kt
+++ app/src/main/java/org/tensorflow/lite/examples/objectdetection/FirstActivity.kt
@@ -2,22 +2,26 @@ package org.tensorflow.lite.examples.objectdetection
 
 //import org.tensorflow.lite.examples.objectdetection.R
 
-import org.tensorflow.lite.examples.objectdetection.MapsActivity
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.media.MediaPlayer
 import android.os.Build
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import org.tensorflow.lite.examples.objectdetection.MainActivity
 
 class FirstActivity : AppCompatActivity() {
     private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
-
+        val phoneNumber = ""
         // Initialize the MediaPlayer instance with the audio file
         mediaPlayer = MediaPlayer.create(this, R.raw.welcome) // Replace "welcome" with the name of your audio file in the res/raw folder
         mediaPlayer.setOnCompletionListener(MediaPlayer.OnCompletionListener {
@@ -25,6 +29,13 @@ class FirstActivity : AppCompatActivity() {
             mediaPlayer.pause()
             mediaPlayer.seekTo(0)
         })
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val emergencyContact = sharedPreferences.getString("emergencyContact", null)
+
+        if (emergencyContact == null) {
+            showEmergencyContactDialog()
+        }
+
 
         // Get references to the buttons
         val btnNextPage: Button = findViewById(R.id.btnNextPage)
@@ -53,6 +64,29 @@ class FirstActivity : AppCompatActivity() {
             val intent = Intent(this@FirstActivity, MapsActivity::class.java)
             startActivity(intent)
         }
+    }
+    private fun showEmergencyContactDialog() {
+        val editText = EditText(this)
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Set Emergency Contact")
+            .setMessage("Please enter an emergency contact number:")
+            .setView(editText)
+            .setCancelable(false)
+            .setPositiveButton("Save") { _, _ ->
+                val contactNumber = editText.text.toString()
+                if (contactNumber.isNotEmpty()) {
+                    saveEmergencyContact(contactNumber)
+                }
+            }
+            .create()
+
+        dialog.show()
+    }
+
+    private fun saveEmergencyContact(contactNumber: String) {
+        val editor = sharedPreferences.edit()
+        editor.putString("emergencyContact", contactNumber)
+        editor.apply()
     }
 
     override fun onResume() {
